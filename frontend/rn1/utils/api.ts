536708391e1f5e28c2f14ad73335a49576a2446d
camilaaylenricardo <163005132+camilaaylenricardo@users.noreply.github.com>
2024-10-14 19:00:08 -0300

    Add files via upload

---
+++ frontend/rn1/utils/api.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost/nodejs/pde/my-project/backend/'; 
+
+export const registerUser = async (username: string, email: string, password: string) => {
+    return await axios.post(`${API_URL}register.php`, {
+        username,
+        email,
+        password,
+    });
+};
+
+export const loginUser = async (email: string, password: string) => {
+    return await axios.post(`${API_URL}other_api.php`, {
+        email,
+        password,
+    });
+};
